09f5767b211bbdc793075b9e341231c8892117d8
EmmaW8 <sjwang@cse.cuhk.edu.hk>
2020-08-16 01:13:31 +0800

    mixup

--- kornia/augmentation/functional.py
+++ kornia/augmentation/functional.py
@@ -896,10 +896,10 @@ def apply_mixup(input: torch.Tensor, labels: torch.Tensor,
     """
     input = _transform_input(input)
     _validate_input_dtype(input, accepted_dtypes=[torch.float16, torch.float32, torch.float64])
-    input_permute = input.index_select(dim=0, index=params['mixup_pairs'].cuda())
-    labels_permute = labels.index_select(dim=0, index=params['mixup_pairs'].cuda())
+    input_permute = input.index_select(dim=0, index=params['mixup_pairs'].to(input.dtype))
+    labels_permute = labels.index_select(dim=0, index=params['mixup_pairs'].to(labels.dtype))
 
-    lam = params['mixup_lambdas'].view(-1, 1, 1, 1).expand_as(input).cuda()
+    lam = params['mixup_lambdas'].view(-1, 1, 1, 1).expand_as(input).to(input.dtype)
     inputs = input * (1 - lam) + input_permute * lam
-    labels = torch.stack([labels.float(), labels_permute.float(), params['mixup_lambdas'].cuda()], dim=-1)
+    labels = torch.stack([labels.float(), labels_permute.float(), params['mixup_lambdas'].to(labels.dtype)], dim=-1)
     return inputs, labels
